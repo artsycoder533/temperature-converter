275a98addce5f46c7e63b588319883da0b9db937
Natasha <njohnson533@gmail.com>
2021-08-06 11:06:24 -0400

    added animation to container border

---
+++ temperature-converter/script.js
@@ -0,0 +1,4 @@
+const userInput = document.getElementById('temp');
+const celciusBtn = document.getElementsById('celcius');
+const fahrenheitBtn = document.getElementsById('fahrenheit');
+const result = document.getElementsById('result');
