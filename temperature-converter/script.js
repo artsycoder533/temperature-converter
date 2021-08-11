939f6b7ac182d3fd086df4cc87b37843260a3f2b
Natasha <njohnson533@gmail.com>
2021-08-10 21:06:45 -0400

    deleted getUnit function, added global variable for user input, program works correctly

--- temperature-converter/script.js
+++ temperature-converter/script.js
@@ -2,61 +2,41 @@ const userInput = document.querySelector(".textbox");
 const convertBtn = document.querySelector('#convert');
 const result = document.querySelector('#result');
 const radioBtns = document.querySelectorAll('input[name="unit"]');
-
+let input;
 
 // add event listener for textbox, target change event once the focus leaves the textbox and the user selects something else
-userInput.addEventListener('change', getInput);
-
+userInput.addEventListener('change', function () {
+    input = this.value;
+});
+    
 //add event listener to convert button
 convertBtn.addEventListener('click', function (e) {
-    e.preventDefault();
-    //check which radio button was selected, call function
-    
+	e.preventDefault();
+	//check which radio button was selected, call function
+	// iterate over the radio button group and check which button is selected, once found get the value of that button
+	radioBtns.forEach(function (radioBtn) {
+		if (radioBtn.checked) {
+            const unit = radioBtn.value;
+			// call corresponding unit converting function
+            if (unit === "celcius") {
+				toFahrenheit(input);
+			} else {
+				toCelcius(input);
+			}
+			return unit;
+		}
+	});
 });
 
-function getInput() {
-    const userTemp = this.value;
-    console.log(userTemp);
-    getUnit(userTemp);
-}
-
-
-function getUnit(input) {
-     // select the group of radio buttons with the same name
-    console.log("inside getUnit, input is :" + input);
-    // iterate over the radio button group and check which button is selected, once found get the value of that button
-    radioBtns.forEach(function (radioBtn) {
-        if (radioBtn.checked) {
-            const unit = radioBtn.value;
-            console.log(unit);
-            // call corresponding unit converting function
-            if (unit === "celcius") {
-                console.log("inside if celcius, input is: " + input);
-                toFahrenheit(input);
-            } else {
-                console.log("inside if fahrenheit, input is: " + input);
-                toCelcius(input);
-            }
-            return unit;
-        }
-    });
-    
-}
 
 function toCelcius(temp) {
-    console.log("inside toCelcius");
     let convertedTemp = (5 / 9) * (temp - 32);
-    console.log(convertedTemp);
     result.textContent = convertedTemp;
-	console.log(typeof convertedTemp);
     return convertedTemp;
 }
 
 function toFahrenheit(temp) {
-    console.log("inside toFahrenheit");
     let convertedTemp = (temp * (9 / 5)) + 32;
-    console.log(convertedTemp);
     result.textContent = convertedTemp;
-    console.log(typeof convertedTemp);
     return convertedTemp;
 }
